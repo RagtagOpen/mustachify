764bdf8a041b98421ae2e7f1aaa67f2861b3f63c
Ian Dees <ian.dees@gmail.com>
2018-01-07 12:28:06 -0600

    Switch to 4 spaces, add file size range, random S3 key name

--- app.py
+++ app.py
@@ -1,49 +1,61 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os, json, boto3
-from settings import *
+import base64
+import boto3
+import json
+import os
+import posixpath
+import uuid
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route("/submit-form/", methods = ["POST"])
+
+@app.route("/submit-form/", methods=["POST"])
 def submit_form():
 
-  # Collect the data posted from the HTML form
-  avatar_url = request.form["avatar-url"]
-  # TODO: Send this over to Rekognition API
+    # Collect the data posted from the HTML form
+    avatar_url = request.form["avatar-url"]
 
-  # For now, we'll just load homepage again.
-  return render_template('home.html'), 201
+    # TODO: Send this over to Rekognition API
+
+    # For now, we'll just load homepage again.
+    return render_template('home.html'), 201
 
 
 @app.route('/sign-s3/')
 def sign_s3():
-  S3_BUCKET = os.environ.get('S3_BUCKET')
-  file_name = request.args.get('file-name')
-  file_type = request.args.get('file-type')
+    s3_bucket = os.environ.get('S3_BUCKET')
+    file_type = request.args.get('file-type')
+    file_name = posixpath.join('raw', base64.urlsafe_b64encode(uuid.uuid4().bytes).decode('utf8'))
 
-  s3 = boto3.client('s3')
+    s3 = boto3.client('s3')
 
-  presigned_post = s3.generate_presigned_post(
-    Bucket = S3_BUCKET,
-    Key = file_name,
-    Fields = {"acl": "public-read", "Content-Type": file_type},
-    Conditions = [
-      {"acl": "public-read"},
-      {"Content-Type": file_type}
-    ],
-    ExpiresIn = 3600
-  )
+    presigned_post = s3.generate_presigned_post(
+        Bucket=s3_bucket,
+        Key=file_name,
+        Fields={
+            "acl": "public-read",
+            "Content-Type": file_type
+        },
+        Conditions=[
+            {"acl": "public-read"},
+            {"key": file_name},
+            {"Content-Type": file_type},
+            ["content-length-range", 1024, 5242880], # 1KB to 5MB
+        ],
+        ExpiresIn=1200
+    )
 
-  return json.dumps({
-    'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-  })
+    return jsonify({
+        'data': presigned_post,
+        'url': posixpath.join('https://s3.amazonaws.com/', s3_bucket, file_name),
+    })
 
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port = port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
