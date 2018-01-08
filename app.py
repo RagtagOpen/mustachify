35c0ccaa0f9b9d78bc500627100941c1e0a850ef
Ian Dees <ian.dees@gmail.com>
2018-01-07 22:35:16 -0600

    Remove transparency

--- app.py
+++ app.py
@@ -16,6 +16,25 @@ def generate_random_id():
     return base64.urlsafe_b64encode(uuid.uuid4().bytes).decode('utf8').rstrip('=')
 
 
+def remove_transparency(im, bg_colour=(255, 255, 255)):
+
+    # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+
+        # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
+        alpha = im.convert('RGBA').split()[-1]
+
+        # Create a new background image of our matt color.
+        # Must be RGBA because paste requires both images have the same format
+        # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
+        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg.paste(im, mask=alpha)
+        return bg
+
+    else:
+        return im
+
+
 class NoFacesFoundException(Exception):
     pass
 
@@ -58,6 +77,7 @@ def apply_mustache(s3_bucket, original_image_url):
 
     # Save the result as a JPEG in memory
     buf = io.BytesIO()
+    im = remove_transparency(im)
     im.save(buf, 'JPEG')
     buf.seek(0)
 
